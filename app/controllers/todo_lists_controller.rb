a3a5f9daaa7b3e43c6a17bd40882bf160825e226
Tania Munoz <tania.m.jimenez24@gmail.com>
2018-10-13 14:35:19 -0500

    authenticate user controller todo list

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,4 +1,5 @@
 class TodoListsController < ApplicationController
+  before_action :authenticate_user!, only: [:show, :edit, :new, :destroy, :index, :create, :update]
   before_action :set_todo_list, only: [:show, :edit, :update, :destroy]
 
   # GET /todo_lists
