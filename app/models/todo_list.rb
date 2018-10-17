eddafde26d8f7206371220c970ba640383cd4a18
Tania Munoz <tania.m.jimenez24@gmail.com>
2018-10-17 12:24:32 -0500

    change model todo list delete comments

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,17 +1,19 @@
-
+require 'csv'
 class TodoList < ApplicationRecord
 	belongs_to :user
 	has_many :todo_tasks, dependent: :destroy
 
 	def self.all_todo_list_task
-	 TodoList.left_joins(:todo_tasks).select("todo_lists.*, todo_tasks.content as task").where(
-  "todo_lists.id > 0 OR
-   todo_tasks.todo_list_id = todo_lists.id"
-   )
+  	TodoList.left_joins(:todo_tasks).select("todo_lists.*, todo_tasks.content as task").where(
+    "todo_lists.id > 0 OR
+     todo_tasks.todo_list_id = todo_lists.id"
+     )
   end
+
   def self.all_todo_list 
   	TodoList.all
   end
+
 	def self.to_csv
   CSV.generate do |csv|
     columns = %w(id title description task)
@@ -21,4 +23,5 @@ class TodoList < ApplicationRecord
     end
   end
  end
+ 
 end
